b10e38c7016830e1fe7ef656b3a12bf5d77838e8
Harry Pierson <harrypierson@outlook.com>
2019-07-12 13:12:41 -0700

    initial commit

---
+++ neo-debug-adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Neo.Debug.Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
