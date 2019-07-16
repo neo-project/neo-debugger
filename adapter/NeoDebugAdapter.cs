5e7bc45205e2184ba8088d248a202afd6fa8d064
Harry Pierson <harrypierson@outlook.com>
2019-07-15 17:42:02 -0700

    adapter running, need netstandard VSCodeDebugProtocol implementation

--- adapter/NeoDebugAdapter.cs
+++ adapter/NeoDebugAdapter.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.Shared.VSCodeDebugProtocol;
+using Microsoft.VisualStudio.Shared.VSCodeDebugProtocol.Messages;
 using System.IO;
 
 namespace Neo.DebugAdapter
@@ -7,12 +8,28 @@ namespace Neo.DebugAdapter
     {
         public NeoDebugAdapter(Stream @in, Stream @out)
         {
+            var p = this.Protocol;
+
             InitializeProtocolClient(@in, @out);
+
+            var q = this.Protocol;
+            ;
         }
 
         public void Run()
         {
             Protocol.Run();
+        }
+
+        protected override InitializeResponse HandleInitializeRequest(InitializeArguments arguments)
+        {
+            this.Protocol.SendEvent(new InitializedEvent());
+            return base.HandleInitializeRequest(arguments);
+        }
+
+        protected override LaunchResponse HandleLaunchRequest(LaunchArguments arguments)
+        {
+            return base.HandleLaunchRequest(arguments);
         }
     }
 }
--- adapter/Program.cs
+++ adapter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.VisualStudio.Shared.VSCodeDebugProtocol;
 using System;
+using System.Collections.Generic;
 
 namespace Neo.DebugAdapter
 {
@@ -10,16 +12,39 @@ namespace Neo.DebugAdapter
         [Option]
         bool Debug { get; }
 
+        static List<string> log = new List<string>();
+
+        const string LOG_FILE = @"C:\Users\harry\neodebug.log";
+        public static void Log(string message, LogCategory category = LogCategory.Trace)
+        {
+            System.IO.File.AppendAllText(LOG_FILE, $"\n{category} {message}");
+        }
+
         private void OnExecute(CommandLineApplication app, IConsole console)
         {
+            if (System.IO.File.Exists(LOG_FILE))
+                System.IO.File.Delete(LOG_FILE);
+
             if (Debug)
             {
                 System.Diagnostics.Debugger.Launch();
+
+                var proc = System.Diagnostics.Process.GetCurrentProcess();
+                var id = System.Diagnostics.Process.GetCurrentProcess().Id;
+
+                ;
             }
 
             NeoDebugAdapter adapter = new NeoDebugAdapter(Console.OpenStandardInput(), Console.OpenStandardOutput());
-            adapter.Protocol.LogMessage += (sender, _args) => System.Diagnostics.Debug.WriteLine(_args.Message);
+            adapter.Protocol.LogMessage += (sender, args) => Log(args.Message, args.Category);
             adapter.Run();
+
+            if (Debug)
+            {
+                adapter.Protocol.WaitForReader();
+            }
+
+            ;
         }
     }
 }
