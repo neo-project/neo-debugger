446d78c26fa0cd32cf2b5b9d3fc2c71df9594ac5
Harry Pierson <harrypierson@outlook.com>
2019-07-15 17:44:42 -0700

    basic extension scaffolding working

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,26 +1,20 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const factory = new NeoContractDebugAdapterDescriptorFactory();
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory("neo-contract", factory));
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "neo-contract-debug" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+class NeoContractDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+		const path = String.raw`C:\Users\harry\Source\neo\seattle\debug\adapter\bin\Debug\netcoreapp2.2\neo-debug-adapter.dll`;
+		return new vscode.DebugAdapterExecutable("dotnet", [path]);
+	}
 }
 
 // this method is called when your extension is deactivated
