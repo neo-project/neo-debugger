78f0a41904549433281dd59f9f3b11514ba3a4f8
Harry Pierson <harrypierson@outlook.com>
2019-08-09 13:29:46 -0700

    provideDebugConfigurations

--- src/extension/src/extension.ts
+++ src/extension/src/extension.ts
@@ -2,12 +2,71 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
+import { join, basename } from 'path';
+import { unwatchFile } from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const configProvider = new NeoContractDebugConfigurationProvider();
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider("neo-contract", configProvider));
+
 	const factory = new NeoContractDebugAdapterDescriptorFactory();
 	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory("neo-contract", factory));
+}
+
+async function* findNeoVmFiles(uri: vscode.Uri) : AsyncIterableIterator<string> {
+	for (var [fileName,fileType] of await vscode.workspace.fs.readDirectory(uri)) {
+		if (fileType === vscode.FileType.File && fileName.endsWith(".avm"))
+		{
+			yield join(uri.fsPath, fileName);
+		}
+
+		if (fileType === vscode.FileType.Directory)
+		{
+			for await (var file of findNeoVmFiles(vscode.Uri.file(join(uri.fsPath, fileName))))
+			{
+				yield file;
+			}
+		}
+	}
+}
+
+class NeoContractDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
+	public async provideDebugConfigurations(folder: WorkspaceFolder | undefined, token?: CancellationToken): Promise<DebugConfiguration[]> {
+		
+		function createConfig(programPath: string | undefined = undefined): DebugConfiguration {
+			return {
+				name: programPath ? basename(programPath) : "NEO Contract",
+				type: "neo-contract",
+				request: "launch",
+				program: "${workspaceFolder}" + (programPath ? programPath : ""),
+				args: [],
+				storage: [],
+				runtime: {
+					witnesses: {
+						"check-result": true
+					}
+				}
+			};
+		}
+		
+		if (folder)
+		{
+			var configs : DebugConfiguration[] = [];
+			for await (var neoVmFile of findNeoVmFiles(folder.uri))
+			{
+				var programPath = neoVmFile.slice(folder.uri.fsPath.length);
+				configs.push(createConfig(programPath));
+			}
+
+			if (configs.length > 0) {
+				return configs;
+			}
+		}
+
+		return [createConfig()];
+	}
 }
 
 class NeoContractDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
