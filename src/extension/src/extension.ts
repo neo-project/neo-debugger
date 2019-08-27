d4df96c66ba054c82f5dfbe88e0e9b0806478673
Harry Pierson <harrypierson@outlook.com>
2019-08-27 13:51:56 -0700

    install debug adapter from vsix if possible

--- src/extension/src/extension.ts
+++ src/extension/src/extension.ts
@@ -2,13 +2,112 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
-import { join, basename, relative } from 'path';
-import { unwatchFile } from 'fs';
-import { create } from 'domain';
+import { join, basename, relative, resolve } from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as cp from 'child_process';
+
+function checkFileExists(filePath: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        fs.stat(filePath, (err, stats) => {
+			if (stats && stats.isFile()) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+}
+
+function deleteFile(filePath: fs.PathLike): Promise<void> {
+	return new Promise((resolve, reject) => {
+		fs.unlink(filePath, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
+function execChildProcess(command: string, workingDirectory: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        cp.exec(command, { cwd: workingDirectory, maxBuffer: 500 * 1024 }, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+            }
+            else if (stderr && stderr.length > 0) {
+                reject(new Error(stderr));
+            }
+            else {
+                resolve(stdout);
+            }
+        });
+    });
+}
+
+function getDebugAdapterFileName(): string {
+	return os.platform() === "win32" ? "neo-debug-adapter.exe" : "neo-debug-adapter";
+}
+
+function getDebugAdapterPath(extension:vscode.Extension<any>): string {
+	return resolve(extension.extensionPath, "adapter", getDebugAdapterFileName());
+}
+
+function getDebugAdapterPackageFileName(extension:vscode.Extension<any>): string {
+	return "neo-debug-adapter." + extension.packageJSON.version + ".nupkg";
+}
+
+function getDebugAdapterPackagePath(extension:vscode.Extension<any>): string {
+	return resolve(extension.extensionPath, getDebugAdapterPackageFileName(extension));
+}
+
+function checkDebugAdapterExists(extension:vscode.Extension<any>): Promise<boolean> {
+	return checkFileExists(getDebugAdapterPath(extension));
+}
+
+function checkDebugAdapterPackageExists(extension:vscode.Extension<any>): Promise<boolean> {
+	return checkFileExists(getDebugAdapterPackagePath(extension));
+}
+
+function inDevelopmentMode() : boolean {
+	// recommended workaround for https://github.com/Microsoft/vscode/issues/10272
+	return vscode.env.sessionId === "someValue.sessionId";
+}
+
+async function processRuntimeDependencies(): Promise<void> {
+	
+	const extension = vscode.extensions.getExtension("ngd-seattle.neo-contract-debug") as vscode.Extension<any>;
+	const debugAdapterExists = await checkDebugAdapterExists(extension);
+	const debugAdapterPackageExists = await checkDebugAdapterPackageExists(extension);
+
+	if (debugAdapterExists) {
+		if (debugAdapterPackageExists) {
+			// cleanup
+			await deleteFile(getDebugAdapterPackagePath(extension));
+		}
+	} else {
+		if (debugAdapterPackageExists) {
+			let response = await execChildProcess(
+				`dotnet tool install neo-debug-adapter --version ${extension.packageJSON.version} --tool-path ./adapter --add-source .`,
+				extension.extensionPath);
+			await deleteFile(getDebugAdapterPackagePath(extension));
+			vscode.window.showInformationMessage(response);
+		} else {
+			if (!inDevelopmentMode()) {
+				vscode.window.showErrorMessage("Neo Debug adapter tool and package are both missing. Please reinstall the extension");
+			}
+		}
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+
+	await processRuntimeDependencies();
+
 	const configProvider = new NeoContractDebugConfigurationProvider();
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider("neo-contract", configProvider));
 
@@ -49,14 +148,29 @@ class NeoContractDebugConfigurationProvider implements vscode.DebugConfiguration
 	}
 }
 
+async function getDebugAdapterCommand(config:vscode.WorkspaceConfiguration) : Promise<[string, string[]]> {
+	var debugAdapterConfig = config.get<string[]>("debug-adapter");
+	if (debugAdapterConfig && debugAdapterConfig.length > 0) {
+		return [debugAdapterConfig[0], debugAdapterConfig.slice(1)];
+	}
+
+	const extension = vscode.extensions.getExtension("ngd-seattle.neo-contract-debug") as vscode.Extension<any>;
+	if (await checkDebugAdapterExists(extension)) {
+		return [getDebugAdapterPath(extension), []];
+	}
+
+	if (inDevelopmentMode()) {
+		const adapterPath = resolve(extension.extensionPath, "..", "adapter");
+		return ["dotnet", ["run", "--project", adapterPath, "--"]];
+	}
+
+	throw new Error("cannot locate debug adapter");
+}
 class NeoContractDebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
-	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+	async createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): Promise<vscode.DebugAdapterDescriptor> {
 		const config = vscode.workspace.getConfiguration("neo-debugger");
-		
-		var debugAdapterConfig = config.get<string[]>("debug-adapter");
-		var cmd = debugAdapterConfig && debugAdapterConfig.length > 0 ? debugAdapterConfig[0] : "neo-debug-adapter";
-		var args : string[] = debugAdapterConfig && debugAdapterConfig.length > 1 ? debugAdapterConfig.slice(1) : [];
+		let [cmd, args] = await getDebugAdapterCommand(config);
 		
 		if (config.get<Boolean>("debug", false))
 		{
