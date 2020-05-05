255982bf5188ac6494e3510909c3b92242845d46
Harry Pierson <harrypierson@ngd.neo.org>
2020-05-04 18:47:26 -0700

    update package.json and change gulpfile from ts -> js

--- src/extension/gulpfile.ts
+++
@@ -1,8 +0,0 @@
-
-import * as gulp from 'gulp';
-import { resetPackageVersionPlaceholder, setPackageVersion } from 'nerdbank-gitversioning';
-
-gulp.task('setversion', function() {
-    resetPackageVersionPlaceholder();
-    return setPackageVersion();
-});
