4b0fea060f3236401188f70fbd0e3feb8952e2c5
Harry Pierson <harrypierson@ngd.neo.org>
2020-05-06 11:25:43 -0700

    ScriptTable.GetScript returns null for missing script

--- src/adapter/ScriptTable.cs
+++ src/adapter/ScriptTable.cs
@@ -12,11 +12,11 @@ namespace NeoDebug
         public void Add(byte[] script) 
             => scripts.Add(Crypto.HashScript(script), script);
 
-        public byte[] GetScript(byte[] scriptHash)
+        public byte[]? GetScript(byte[] scriptHash)
             => GetScript(new UInt160(scriptHash));
 
-        public byte[] GetScript(in UInt160 scriptHash)
+        public byte[]? GetScript(in UInt160 scriptHash)
             => scripts.TryGetValue(scriptHash, out var script)
-                ? script : Array.Empty<byte>();
+                ? script : null;
     }
 }
